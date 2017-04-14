851e02ae256909f11e5c2ba600fc18ce0e627617
Magnus <magnuskollip@hotmail.dk>
2017-04-14 16:35:22 +0200

    Testy

---
+++ src/brugerautorisation/transport/rmi/Brugeradminklient.java
@@ -0,0 +1,24 @@
+package brugerautorisation.transport.rmi;
+
+import brugerautorisation.data.Diverse;
+import brugerautorisation.data.Bruger;
+import java.rmi.Naming;
+
+public class Brugeradminklient {
+	public static void main(String[] arg) throws Exception {
+//		Brugeradmin ba =(Brugeradmin) Naming.lookup("rmi://localhost/brugeradmin");
+		Brugeradmin ba = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+
+    //ba.sendGlemtAdgangskodeEmail("jacno", "Dette er en test, husk at skifte kode");
+		//ba.ændrAdgangskode("jacno", "kodenj4gvs", "xxx");
+		Bruger b = ba.hentBruger("s153086", "nusnus");
+		System.out.println("Fik bruger = " + b);
+		System.out.println("Data: " + Diverse.toString(b));
+		// ba.sendEmail("jacno", "xxx", "Hurra det virker!", "Jeg er så glad");
+
+		//ba.ændrAdgangskode("s153086", "kodek3plrj", "nusnus");
+
+		//String webside = (String) ba.getEkstraFelt("jacno", "xxx", "webside");
+		//System.out.println("webside = " + webside);
+	}
+}
